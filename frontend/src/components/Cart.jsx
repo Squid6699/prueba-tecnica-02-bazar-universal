17420efebc00b3074b6f1c024b59d782c28092f0
Lil Squid <80144515+Squid6699@users.noreply.github.com>
2024-08-11 01:32:12 -0700

    Agregar o eliminar en el carrito

--- frontend/src/components/Cart.jsx
+++ frontend/src/components/Cart.jsx
@@ -2,17 +2,17 @@ import "../css/cart.css"
 import { useCart } from "../hooks/useCart";
 import Header from "../components/Header"
 import { useNavigate } from "react-router-dom";
+import { useState } from "react";
 
 function Cart(){
     const {cartMapped} = useCart();
     const navigate = useNavigate();
+    const [count, setCount] = useState(1);
 
     const hadnleItemSelected = (id) => {
         navigate(`/items/${id}`);
     }
     return(
-
-        
         <>
             <Header spot={"cart"}/>
 
@@ -34,6 +34,13 @@ function Cart(){
                                         <div className="rating-stars-cart" style={{ '--rating': item.rate }}></div>
                                     </div>
                                 </li>
+                                 
+                                <div className="counter">
+                                    <div className="count-less"><button>-</button></div>
+                                    <div className="count-item-cart"><input type="number" value={count}/></div>
+                                    <div className="count-more"><button onClick={() => setCount(count+1)}>+</button></div>
+                                </div>
+                                
                             </div>
                         ))
                     : <strong className="form-search">CARRITO VACIO</strong>
